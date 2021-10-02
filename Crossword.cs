56c952bc4a0f711499bbd8186e9baa7348b8da25
VR CORP <head@vrcorp.ru>
2021-10-02 21:01:03 +0700

    Updated

--- Crossword.cs
+++ Crossword.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using Functions.Crossword;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
@@ -11,54 +12,63 @@ public class Crossword : MonoBehaviour
     public Text txtWord;
     public Text txtResult;
     public TextAsset resourceDictionaryJson;
-    private DictionaryRus _dictionary;
-    private char [] _hiddenWord;
-    
-    private void Start()
-    { 
-        _dictionary = DictionaryRus.CreateFromJson(resourceDictionaryJson.text);
-        
-        _hiddenWord = GetRandomWord();
-        print(new string(_hiddenWord));
 
-        foreach (char letter in _hiddenWord)
+    private DictionaryRus _dictionary;
+    private char[] _hiddenWord;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _dictionary = DictionaryRus.CreateFromJson(resourceDictionaryJson.text);
+        _hiddenWord = GetRandom();
+
+        foreach (var letter in _hiddenWord)
         {
             txtWord.text += "*";
         }
     }
 
-    private char [] GetRandomWord()
-    {
-        int rnd = Random.Range(0, _dictionary.words.Length);
-        return _dictionary.words[rnd].ToCharArray();
-    }
-
-    public void GuessLetter()
+    public void GetLetter()
     {
         if (inputField.text.Length == 0) return;
         char letter = inputField.text.ToLower()[0];
 
+        int counter = 0;
+        
         for (int i = 0; i < _hiddenWord.Length; i++)
         {
             if (_hiddenWord[i] == letter)
             {
-                char [] openWord = txtWord.text.ToCharArray();
-                openWord[i] = _hiddenWord[i];
-                txtWord.text = new string(openWord);
+                counter++;
+                char[] openword = txtWord.text.ToCharArray();
+                openword[i] = letter;
+                txtWord.text = new string(openword);
             }
         }
 
-        txtResult.text = $"Найдено {WordsHandler.GetLetterCount(letter, new string (_hiddenWord))} букв";
+        txtResult.text = $"Найдено {counter} букв";
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 
-    [Serializable]
-    public class DictionaryRus
+    private char[] GetRandom()
     {
-        public string [] words;
-    
-        public static DictionaryRus CreateFromJson (string jsonString)
-        {
-            return JsonUtility.FromJson<DictionaryRus>(jsonString);
-        }
+        int rnd = Random.Range(0, _dictionary.words.Length);
+        return _dictionary.words[rnd].ToCharArray();
+    }
+}
+
+[Serializable]
+public class DictionaryRus
+{
+    public string[] words;
+
+    public static DictionaryRus CreateFromJson(string json)
+    {
+        return JsonUtility.FromJson<DictionaryRus>(json);
     }
 }
